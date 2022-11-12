84fd973f36eea5c23a32c879472381c6101dc354
zurbash <duta1101@bk.ru>
2022-11-12 19:23:43 +0400

    chore: removed unnecessary code

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import LandingPage from "../pages/LandingPage.vue";
+import LandingPage from "@/pages/LandingPage.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,14 +8,6 @@ const router = createRouter({
       path: "/",
       name: "landing",
       component: LandingPage,
-    },
-    {
-      // path: "/about",
-      // name: "about",
-      // // route level code-splitting
-      // // this generates a separate chunk (About.[hash].js) for this route
-      // // which is lazy-loaded when the route is visited.
-      // component: () => import("../pages/AboutView.vue"),
     },
   ],
 });
