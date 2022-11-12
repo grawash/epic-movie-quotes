f4ea7e1654f2d994911bd847bae003925294299b
zurbash <duta1101@bk.ru>
2022-11-12 15:58:51 +0400

    chore: removed unnecessary files

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="bg-black">
+    <p class="text-[#DDCCAA]">Movie quotes</p>
+    <div>
+      <button>bell</button>
+      <button>lang</button>
+      <button>log out</button>
+    </div>
+  </div>
+</template>
---
+++ src/pages/LandingPage.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>HEADER</h1>
+  </div>
+  <div>
+    <p>CONTENT</p>
+  </div>
+</template>
