b36a20fb1f3b887ab702e96e1bd1053e999ff906
zurbash <duta1101@bk.ru>
2022-12-06 12:46:13 +0400

    feat: added component for displaying password rules

--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -24,7 +24,7 @@
           type="text"
           id="newName"
           rule="required|min:2"
-          placeholder="Nino Tabagari"
+          :placeholder="user.name"
           label="new username"
           class="pt-10"
           @input="changed = true"
@@ -62,40 +62,10 @@
           :editable="!user.google_authenticated"
           :readonly="true"
         />
-        <div
+        <password-rule-notice
           v-if="passwordReadOnly"
-          class="p-6 border border-[#CED4DA] rounded-lg border-opacity-50 mt-9 max-w-[528px]"
-        >
-          <p>Passwords should contain:</p>
-          <ul>
-            <li class="flex items-center pt-4">
-              <span
-                class="before:content-['\2022'] text-xl pr-[6px]"
-                :class="[
-                  {
-                    neutral: passwordValue === '',
-                    valid: passwordValue.length >= 8,
-                    invalid: passwordValue.length < 8,
-                  },
-                ]"
-              ></span
-              >8 or more characters
-            </li>
-            <li class="flex items-center pt-1">
-              <span
-                class="before:content-['\2022'] text-xl pr-[6px]"
-                :class="[
-                  {
-                    neutral: passwordValue === '',
-                    valid: passwordValue.length <= 15,
-                    invalid: passwordValue.length > 15,
-                  },
-                ]"
-              ></span
-              >max 15 lowercase characters
-            </li>
-          </ul>
-        </div>
+          :passwordValue="passwordValue"
+        />
         <profile-page-input
           v-if="passwordReadOnly"
           name="password"
@@ -138,6 +108,7 @@
 import { Form } from "vee-validate";
 import ProfilePageInput from "@/components/inputs/ProfilePageInput.vue";
 import PhotoInput from "@/components/inputs/PhotoInput.vue";
+import PasswordRuleNotice from "@/components/PasswordRuleNotice.vue";
 import AddMailIcon from "@/components/icons/AddMailIcon.vue";
 import { ref } from "vue";
 import { useRoute } from "vue-router";
