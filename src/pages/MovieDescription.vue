efdb2a4e55b26bc57344760a4a14c4f5196714d6
zurbash <duta1101@bk.ru>
2022-12-08 22:39:22 +0400

    feat: search movies functionality

--- src/pages/MovieDescription.vue
+++ src/pages/MovieDescription.vue
@@ -16,18 +16,27 @@
     </div>
     <div class="col-start-6 col-end-10 flex flex-col">
       <div class="flex flex-col">
-        <p class="font-medium text-2xl text-[#DDCCAA]">{{ movie.title }}</p>
+        <div class="flex items-center">
+          <p class="font-medium text-2xl text-[#DDCCAA]">{{ movie.title }}</p>
+          <div
+            class="bg-[#24222F] flex ml-auto p-[10px] pl-7 pr-7 gap-[25px] items-center rounded-lg"
+          >
+            <pencil-icon class="cursor-pointer hover:scale-110" />
+            <div class="h-[16px] w-[1px] bg-[#6C757D]"></div>
+            <trash-can-icon class="cursor-pointer hover:scale-110" />
+          </div>
+        </div>
         <div class="flex flex-wrap mt-6">
           <div
             v-for="genre in genres"
-            class="items-center h-min p-[2px] pl-[6px] pr-[6px] rounded mr-2 bg-[#6C757D]"
+            class="items-center h-min p-[2px] pl-[6px] mb-2 pr-[6px] rounded mr-2 bg-[#6C757D]"
           >
             <p class="capitalize font-bold text-lg">
               {{ genre.name }}
             </p>
           </div>
         </div>
-        <div class="text-lg ml-3 mt-5">
+        <div class="text-lg ml-3 mt-3">
           <p class="font-bold mt-5">
             <span class="text-[#CED4DA]">Director: </span> {{ movie.director }}
           </p>
@@ -39,7 +48,8 @@
 </template>
 <script setup>
 import AddMailIcon from "@/components/icons/AddMailIcon.vue";
-// import LoopIcon from "@/components/icons/LoopIcon.vue";
+import TrashCanIcon from "@/components/icons/TrashCanIcon.vue";
+import PencilIcon from "@/components/icons/PencilIcon.vue";
 // import CommentIcon from "@/components/icons/CommentIcon.vue";
 
 // import AddMovieForm from "@/components/AddMovieForm.vue";
--- src/pages/MoviesList.vue
+++ src/pages/MoviesList.vue
@@ -1,17 +1,25 @@
 <template>
   <add-movie-form v-if="addMovieModal" @closeModals="closeModals" />
+
   <div class="col-start-4 col-end-13 flex flex-col gap-14">
     <div class="flex items-center">
       <p class="font-medium text-2xl">
         My list of movies ({{ movieList.length }})
       </p>
-      <div class="ml-auto flex">
+      <div class="ml-auto flex" @click.stop="">
         <button
           class="mr-8 font-normal text-xl text-[#CED4DA] flex items-center"
+          @click="searchInput = true"
         >
           <loop-icon class="mr-2" />
           Search
         </button>
+        <input
+          v-if="searchInput"
+          v-model="searchValue"
+          type="text"
+          class="rounded-lg p-2 bg-transparent mr-8 border border-[#6C757D]"
+        />
         <button
           @click="addMovieModal = true"
           class="rounded-lg font-normal text-xl p-[9px] pl-4 pr-4 bg-[#E31221] flex items-center"
@@ -23,7 +31,7 @@
     </div>
     <div class="grid grid-cols-3 auto-rows-[430px] gap-12 grow mb-10">
       <div
-        v-for="movie in movieList"
+        v-for="movie in filteredMovies"
         class="flex flex-col hover:scale-105 cursor-pointer"
         @click="showMovieDescription(movie.id)"
       >
@@ -45,7 +53,7 @@ import CommentIcon from "@/components/icons/CommentIcon.vue";
 
 import AddMovieForm from "@/components/AddMovieForm.vue";
 import axios from "@/config/axios/index.js";
-import { ref } from "vue";
+import { ref, onUnmounted, computed } from "vue";
 import { useRoute } from "vue-router";
 import { useRouter } from "vue-router";
 const router = useRouter();
@@ -53,6 +61,12 @@ const route = useRoute();
 const emit = defineEmits(["profileNotice"]);
 const movieList = ref([]);
 const addMovieModal = ref(false);
+const searchInput = ref(false);
+const searchValue = ref("");
+window.onclick = removeSearch;
+onUnmounted(() => {
+  window.onclick = "";
+});
 function getImageUrl(movie) {
   let replaced = movie.replace("public", "storage");
   return "http://127.0.0.1:8000/" + replaced;
@@ -63,6 +77,17 @@ function closeModals() {
 function showMovieDescription(movieId) {
   router.push({ name: "movie-description", params: { movieId: movieId } });
 }
+function removeSearch() {
+  searchInput.value = false;
+}
+const filteredMovies = computed(() => {
+  if (movieList.value.length != 0) {
+    let filtered = movieList.value.filter((movie) =>
+      movie.title.toLowerCase().includes(searchValue.value.toLowerCase())
+    );
+    return filtered;
+  } else return movieList.value;
+});
 axios
   .get("movies-list")
   .then(({ data }) => {
